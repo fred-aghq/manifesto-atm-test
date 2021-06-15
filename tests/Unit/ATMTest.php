6a31f38fac47c90c33a8b730ad6897049aeabae7
Fred Spooner <fred@aghq.uk>
2021-06-15 19:00:09 +0100

    test: skeleton framework of basic expectations of app

---
+++ tests/Unit/ATMTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ATMTest extends TestCase
+{
+    /**
+     * The ATM cannot dispense more money than it holds.
+     * The customer cannot withdraw more funds then they have access to.
+     * The ATM should not dispense funds if the pin is incorrect.
+     * The ATM should not expose the customer balance if the pin is incorrect.
+     * The ATM should only dispense the exact amounts requested.
+     */
+
+    public function test_it_does_not_dispense_more_money_than_it_holds()
+    {
+        $this->assertTrue(false, "test not written");
+    }
+}
---
+++ tests/Unit/CustomerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CustomerTest extends TestCase
+{
+    /**
+     *
+     * The customer cannot withdraw more funds then they have access to.
+     */
+    public function test_it_cannot_withdraw_more_funds_than_it_has_access_to()
+    {
+        $this->assertTrue(false, 'test not written yet');
+    }
+}
