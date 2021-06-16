aae8ac485ec73a31e39978f4b300d4f30d4e8cf4
Fred Spooner <fred@aghq.uk>
2021-06-16 12:09:52 +0100

    test: basic test of valid login and successful withdrawal

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -13,7 +13,30 @@ class CustomerTest extends TestCase
      */
     public function test_it_cannot_withdraw_more_funds_than_it_has_access_to()
     {
+        $accountNumber = 12345678;
+        $pin = 1234;
+        $cashAvailable = 8000;
+        $customerBalance = 250;
+        $customerOverdraftAvailibility = 100;
 
+        $this->artisan('manifesto:atm')
+            ->expectsOutput($cashAvailable)
+            ->expectsQuestion('Enter account number', '12345678')
+            ->expectsQuestion('Enter PIN', '1234')
+            ->expectsOutput($accountNumber . ' ' . $pin . ' ' . $pin)
+            ->expectsChoice(
+                'Select an operation',
+                'B',
+                ['(B)alance enquiry', '(W)ithdraw cash']
+            )
+            ->expectsOutput($customerBalance . ' ' . $customerOverdraftAvailibility)
+            ->expectsChoice(
+                'Select an operation',
+                'W',
+                ['(B)alance enquiry', '(W)ithdraw cash']
+            )
+            ->expectsQuestion('Withdrawal amount', 300)
+            ->assertExitCode(ATM::WITHDRAWAL_SUCCESS);
     }
 
     public function test_it_can_withdraw_with_successful_login_and_valid_amount()
@@ -33,7 +56,6 @@ class CustomerTest extends TestCase
                 'Select an operation',
                 'B',
                 ['(B)alance enquiry', '(W)ithdraw cash']
-
             )
             ->expectsOutput($customerBalance . ' ' . $customerOverdraftAvailibility)
             ->expectsChoice(
