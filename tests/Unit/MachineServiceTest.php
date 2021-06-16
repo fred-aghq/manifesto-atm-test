f66aad99403d0ccac950f7d856fdc569a61e8898
Fred Spooner <fred@aghq.uk>
2021-06-16 16:39:03 +0100

    test: update test expectations

--- tests/Unit/MachineServiceTest.php
+++ tests/Unit/MachineServiceTest.php
@@ -2,23 +2,24 @@
 
 namespace Tests\Unit;
 
-use App\Console\Commands\ATM;
+use App\Exceptions\Customer\InvalidAccountNumberException;
+use App\Exceptions\Customer\InvalidPinException;
 use App\Exceptions\Machine\MachineErrorException;
 use App\Models\Customer;
 use App\Models\Machine;
+use App\Repositories\CustomerRepository;
 use App\Repositories\MachineRepository;
-use App\Services\ATM\MachineService;
 use App\Services\ATM\MachineServiceInterface;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\DB;
 use Mockery\MockInterface;
-use Tests\CreatesApplication;
 use Tests\TestCase;
 
 class MachineServiceTest extends TestCase
 {
     use RefreshDatabase;
+
+    private $unit;
 
     protected function setUp(): void
     {
@@ -26,13 +27,39 @@ class MachineServiceTest extends TestCase
         Machine::factory()->create();
     }
 
-    /**
-     * The ATM cannot dispense more money than it holds.
-     * The customer cannot withdraw more funds then they have access to.
-     * The ATM should not dispense funds if the pin is incorrect.
-     * The ATM should not expose the customer balance if the pin is incorrect.
+    /*
      * The ATM should only dispense the exact amounts requested.
      */
+
+    public function test_it_only_dispenses_the_exact_amounts_required()
+    {
+        $machine = Machine::first();
+        $customer = Customer::factory()->create();
+
+        $totalCash = $machine->total_cash;
+        $withdrawalAmount = 100;
+
+        $this->mock(
+            MachineRepository::class,
+            function (MockInterface $mock) use ($machine) {
+                $mock->shouldReceive('getMachine')
+                    ->andReturn($machine);
+            });
+
+        $this->mock(
+            CustomerRepository::class,
+            function (MockInterface $mock) use ($customer) {
+                $mock->shouldReceive('findByAccountNumber')
+                    ->andReturn($customer);
+            }
+        );
+
+        $unit = App::make(MachineServiceInterface::class);
+
+        $unit->login($customer->account_number, $customer->pin);
+        $unit->withdrawCash($withdrawalAmount);
+        $this->assertEquals($totalCash - $withdrawalAmount, $machine->refresh()->total_cash);
+    }
 
     public function test_it_does_not_dispense_more_money_than_it_holds()
     {
@@ -43,17 +70,10 @@ class MachineServiceTest extends TestCase
         $machine->total_cash = $totalCash;
         $machine->save();
 
-        $this->mock(
-            MachineRepository::class,
-            function(MockInterface $mock) use ($machine) {
-                $mock->shouldReceive('getMachine')
-                    ->andReturn($machine);
-            });
-
-        $unit = App::make(MachineServiceInterface::class);
-
         // When I try to withdraw more cash than is available, then a Machine Error Exception is thrown
         $this->expectException(MachineErrorException::class);
+
+        $unit = App::make(MachineServiceInterface::class);
         $unit->withdrawCash(($machine->total_cash + 100));
 
         // And the machine's total cash does not change.
@@ -61,20 +81,17 @@ class MachineServiceTest extends TestCase
         $this->assertEquals($totalCash, $machine->total_cash);
     }
 
+    public function test_it_does_not_continue_if_account_number_not_valid()
+    {
+        $this->expectException(InvalidAccountNumberException::class);
+    }
+
     public function test_it_does_not_dispense_funds_if_pin_is_incorrect()
     {
-        $customer = Customer::factory()->create();
-        $unit = App::make(MachineServiceInterface::class);
-        $this->expectException();
-
+        $this->expectException(InvalidPinException::class);
     }
 
     public function test_it_does_not_expose_customer_balance_if_pin_is_incorrect()
-    {
-        $this->assertTrue(false, "test not written");
-    }
-
-    public function test_it_displays_the_total_cash_held_on_the_first_line()
     {
         $this->assertTrue(false, "test not written");
     }
