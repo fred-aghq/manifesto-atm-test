e9df58e93a97e202f89e3ec3b9a2c07d9280ae0e
Fred Spooner <fred@aghq.uk>
2021-06-16 12:15:43 +0100

    feat: flesh out MachineService, throw relevant Exceptions in appropriate methods

--- app/Services/ATM/MachineService.php
+++ app/Services/ATM/MachineService.php
@@ -2,20 +2,43 @@
 
 namespace App\Services\ATM;
 
+use App\Exceptions\Customer\FundsErrorException;
+use App\Exceptions\Customer\InvalidPinException;
+use App\Exceptions\Machine\MachineErrorException;
 
 class MachineService implements MachineServiceInterface
 {
-    public function withdrawCash(){}
-    public function getAccountBalance(): int {
-        return 500 . ' ' . 100;
+    public function withdrawCash(int $amount){
+        $this->validateWithdrawal($amount);
     }
-    public function getOverdraftAvailability(): int {}
-    public function validatePin(int $pin) {}
-    public function validateAccountNumber(int $accountNumber) {}
+
+    public function getAccountBalance(): int {
+        // @TODO: retrieve customer balance from db
+        return 500;
+    }
+    public function getOverdraftAvailability(): int {
+        // @TODO: retrieve customer overdraft from db
+        return 100;
+    }
+
+    public function validatePin(int $pin) {
+        throw new InvalidPinException();
+    }
+    public function validateAccountNumber(int $accountNumber) {
+        throw new InvalidAccountNumberException();
+    }
 
     public function validateLogin(int $accountNumber, int $pin) {
-        $this->validateAccountNumber($accountNumber);
-        $this->validatePin($pin);
+        return (
+            $this->validateAccountNumber($accountNumber)
+            && $this->validatePin($pin)
+        );
+    }
+
+    public function validateWithdrawal(int $amount)
+    {
+        throw new FundsErrorException();
+        throw new MachineErrorException();
     }
 
     public function getTotalCashAvailable(): int
